5276422c35fed9ee2fa759b447f42e2dee156c7f
gkaveri <gkaveri676@gmail.com>
2023-11-25 13:52:56 +0000

    All test cases are getting failed?

---
+++ src/components/SuggestionItem/index.js
@@ -0,0 +1,22 @@
+// Write your code here
+import './index.css'
+
+const SuggestionItem = props => {
+    const {suggestionDetails, updateSearchInput} = props 
+    const {suggestion} = suggestionDetails 
+
+    onClickSuggestion = () => {
+        updateSearchInput(suggestion)
+    }
+
+    return (
+        <li className="suggestion-item">
+          <p className="suggestion-text">{suggestion}</p>
+           <button className="arrow-button" onClick={onClickSuggestion}>
+            <img src="https://assets.ccbp.in/frontend/react-js/diagonal-arrow-left-up.png"
+              alt="arrow" className="arrow"/>
+           </button>
+        </li>
+    )
+}
+export default SuggestionItem
